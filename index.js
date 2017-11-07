22ad23b0e95f0068f920ebeef09daa34642e3c96
Hallee Palmer <pi.is.good.3.141@gmail.com>
2017-11-07 03:32:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,3 +28,7 @@ function prependKitten(name){
   var newKitten=[name,...kittens]
   return newKitten
 }
+function removeLastKitten(){
+  var newKitten=kitten.slice(0, kitten.length-1)
+  return newKitten
+}
